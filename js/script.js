db54541ff4952b28455f050b5c74d954eabdaca3
FCorona89 <124465720+FCorona89@users.noreply.github.com>
2023-02-04 22:18:29 -0800

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,8 @@
+$(document).ready(function() {
+	
+		$("button").on("click", function() {
+			alert('We are currently working to add more club links. Please select from from available options. Sorry for the inconvinience.');
+			
+		});
+	
+});
